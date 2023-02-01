c93b50ae377d9613ec140132edeeefc8e5a78af5
BRosencrans <brandon.rosencrans@gmail.com>
2023-02-01 13:30:00 -0800

    added  show all employees function

--- index.js
+++ index.js
@@ -111,23 +111,42 @@ const dbConnection = mySql.createConnection({
     });
   };
   
-  //Shows all departments in the company
+  //function to shows all departments in the company
   showAllDepartments = () => {
     console.log("Now showing all departments in the company...\n");
-    dbConnection.query(`SELECT department.id AS id, department_name AS department FROM department`, (err, rows) => {
+    dbConnection.query(`SELECT department.id AS id, department_name AS department FROM department`, (err, data) => {
       if (err) throw err;
-      console.table(rows);
+      console.table(data);
       companyMenu();
     });
   };
-//shows all job titles in the company
+//function to show all job titles in the company
   showAllJobTitles = () => {
     console.log("Now showing all job titles in the company...\n");
     dbConnection.query(`SELECT role.id, role.job_title AS title, department.department_name AS department
     FROM role
-    INNER JOIN department ON role.department_id = department.id`, (err, rows) => {
+    INNER JOIN department ON role.department_id = department.id`, (err, data) => {
       if (err) throw err; 
-      console.table(rows); 
+      console.table(data); 
       companyMenu();
     })
+  };
+//function to show all employees of the company
+showAllEmployees = () => {
+    console.log("Now showing all employees of the company...\n"); 
+    dbConnection.query(`SELECT employee.id, 
+    employee.first_name, 
+    employee.last_name, 
+    role.job_title AS title, 
+    department.department_name AS department,
+    role.salary, 
+    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id`, (err, data) => {
+      if (err) throw err; 
+      console.table(data);
+      companyMenu();
+    });
   };

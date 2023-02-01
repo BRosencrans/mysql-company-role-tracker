a207d46cb6a7584cd213a8f5aff49e8c99f57d14
BRosencrans <brandon.rosencrans@gmail.com>
2023-01-31 21:20:38 -0800

    added inquirer function that brings up a list of all possible actions

--- index.js
+++ index.js
@@ -20,7 +20,93 @@ const dbConnection = mySql.createConnection({
    connectionEstablished= () => {
     console.log("###################################")
     console.log("#                                 #")
-    console.log("#        Company Roles            #")
+    console.log("#    Company Roles and Budget     #")
     console.log("#                                 #")
     console.log("###################################")
+    companyMenu();
+  };
+
+  //inquirer prompt that brings up all possible actions
+  const companyMenu = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'choices', 
+        message: "Hello what can i do for you today?",
+        choices: ["View all Company departments", 
+                  "View all job titles", 
+                  "View all employees", 
+                  "Add a new department", 
+                  "Add a new job title", 
+                  "Add a new employee", 
+                  "Update an employee job title",
+                  "Update an employee's manager",
+                  "View employees by different departments",
+                  "Delete a department from the database",
+                  "Delete a job title from the database",
+                  "Delete an employee from the database",
+                  "View Company budgets",
+                  "Exit"
+                ]
+      }
+    ])//all the choice routes
+      .then((answers) => {
+        const { choices } = answers; 
+  
+        if (choices === "View all Company departments") {
+          showDepartments();
+        }
+  
+        if (choices === "View all job titles") {
+          showJobTitles();
+        }
+  
+        if (choices === "View all employees") {
+          showEmployees();
+        }
+  
+        if (choices === "Add a new department") {
+          addNewDepartment();
+        }
+  
+        if (choices === "Add a new job title") {
+          addNewJobTitle();
+        }
+  
+        if (choices === "Add a new employee") {
+          addNewEmployee();
+        }
+  
+        if (choices === "Update an employee job title") {
+          updateEmployeeJobTitle();
+        }
+  
+        if (choices === "Update an employee's manager") {
+          updateEmpManager();
+        }
+  
+        if (choices === "View employees by different departments") {
+          showEmployeeDepartment();
+        }
+  
+        if (choices === "Delete a department from the database") {
+          deleteDepartmentFromDb();
+        }
+  
+        if (choices === "Delete a job title from the database") {
+          deleteJobTitleFromDb();
+        }
+  
+        if (choices === "Delete an employee from the database") {
+          deleteEmployeeFromDb();
+        }
+  
+        if (choices === "View Company budgets") {
+          viewCompanyBudget();
+        }
+  
+        if (choices === "Exit") {
+          dbConnection.end()
+      };
+    });
   };
